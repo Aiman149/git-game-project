09efbac6f3b369e391c5613fbd56b396b8470df3
Aimana <aman.salman990@gmail.com>
2024-01-16 22:45:00 +0800

    Adjust-FrontEnd

--- Box-Front/script.js
+++ Box-Front/script.js
@@ -4,9 +4,7 @@ boxes[0].addEventListener('click', () => {
     window.location.href = 'darjah.html';
 });
 
-// Add click event listeners for the remaining boxes (boxes 2 to 6) if needed
 for (let i = 1; i < boxes.length; i++) {
     boxes[i].addEventListener('click', () => {
-        // Handle other box clicks here
     });
 }
